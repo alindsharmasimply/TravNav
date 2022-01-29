dedf5867b5227277e43592b1200bb662475520bf
= <=>
2022-01-29 17:05:28 +0530

    Last Try

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,9 +10,7 @@ import Login from "./components/login";
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl";
 
-// @ts-ignore
 // eslint-disable-next-line import/no-webpack-loader-syntax
-// eslint import/no-webpack-loader-syntax
 mapboxgl.workerClass =
   require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
