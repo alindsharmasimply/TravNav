1e14dab26a358b3743cda913ec5efc2e81f25dff
= <=>
2022-01-29 16:22:04 +0530

    Worker Loader issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,7 @@ import axios from "axios";
 import "./App.css";
 import Register from "./components/register";
 import Login from "./components/login";
-import "mapbox-gl/dist/mapbox-gl.css";
-import mapboxgl from "mapbox-gl";
+import mapboxgl from "!mapbox-gl";
 // @ts-ignore
 // eslint-disable-next-line import/no-webpack-loader-syntax, import/no-unresolved
 
